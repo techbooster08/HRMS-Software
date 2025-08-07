525a0647478c69284fc8419fb204a4ea4acb4777
techbooster08 <akashpawar.dev@gmail.com>
2025-08-07 18:47:23 +0530

    second day work done successfully!

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,13 @@
 import Hero from '@/components/public-website/Hero';
 import Features from '@/components/public-website/Fetaures';
+import HowItWorks from '@/components/public-website/HowItsWork';
+import SoftwareInAction from '@/components/public-website/SoftwareInAction';
 export default function Home() {
   return (<>
   <Hero />
   <Features />
+  <HowItWorks/>
+  <SoftwareInAction />
   </>
   );
 }
